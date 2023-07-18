750180ef263dff93a16e5978ec0faf28d1fea06a
Henrique Curi <henriquecuridemiranda@hotmail.com>
2023-07-18 12:36:41 -0300

    Adicionar casos de teste

--- src/test/user.test.js
+++ src/test/user.test.js
@@ -17,16 +17,41 @@ describe('/auth', () => {
   });
 
   it('Deve retornar um erro ao tentar cadastrar um usuário sem enviar todos os campos necessários', async () => {
-    // Implementar
+    const user = {
+      email: 'teste@ufla',
+      senha: '123',
+    };
+
+    const res = await request(app).post(`/user/register`).send(user);
+
+    expect(res.statusCode).toEqual(500);
+    expect(res.body).toHaveProperty('error', 'Campos faltando');
   });
 
-  it('Deve retornar erro de senha inválida ao fornecer uma senha incorreta', async () => {
-    // Implementar
+  it('Deve retornar erro de senha inválida ao fornecer senha incorreta', async () => {
+    const user = {
+      email: 'victor@ufla',
+      senha: 'senha_incorreta',
+    };
+
+    const res = await request(app).post('/user/login').send(user);
+
+    expect(res.status).toBe(401);
+    expect(res.body).toHaveProperty('error', 'Senha incorreta');
   });
 
   it('Deve retornar erro de usuário não encontrado ao fornecer um email não registrado', async () => {
-    // Implementar
+    const user = {
+      email: 'email_nao_registrado',
+      senha: '123',
+    };
+
+    const res = await request(app).post('/user/login').send(user);
+
+    expect(res.status).toBe(404);
+    expect(res.body).toHaveProperty('error', 'Usuário não encontrado');
   });
+
 });
 
 afterAll(() => {
